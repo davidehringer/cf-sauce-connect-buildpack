796b3ac60793951d1e6e506c1e411c715841241f
dehringer <david.ehringer@gmail.com>
2014-04-23 20:02:31 -0400

    customizing compile

--- bin/start.sh
+++ bin/start.sh
@@ -3,9 +3,7 @@
 touch $HOME/logs/sc.log
 (tail -f -n 0 $HOME/logs/sc.log &)
 
+echo "Launching Sauce Connect tunnel"
 bin/sc --user ETSLF --api-key 633246bc-bb5f-4986-a68c-4c6b7bcc4551 --proxy www-proxy.lmig.com:80 --proxy-tunnel --se-port $PORT --verbose --logfile $HOME/logs/sc.log >> $HOME/logs/sc.log 2>&1 &
 
-echo "Kicked off sc..." 
-echo "port: $PORT"
-
-env
+echo "Tunnel launched on port $PORT"
