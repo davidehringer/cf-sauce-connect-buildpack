79d289620eb7666f40a6aa377b1dd6a01fc2e7a9
dehringer <david.ehringer@gmail.com>
2014-04-23 20:26:25 -0400

    customizing compile

--- bin/start.sh
+++ bin/start.sh
@@ -4,7 +4,7 @@ touch $HOME/logs/sc.log
 (tail -f -n 0 $HOME/logs/sc.log &)
 
 echo "Launching Sauce Connect tunnel"
-bin/sc --user ETSLF --api-key 633246bc-bb5f-4986-a68c-4c6b7bcc4551 --proxy www-proxy.lmig.com:80 --proxy-tunnel --se-port $PORT -z 60 --logfile $HOME/logs/sc.log >> $HOME/logs/sc.log 2>&1 &
+bin/sc --user $SAUCE_USER --api-key $SAUCE_API_KEY --proxy www-proxy.lmig.com:80 --proxy-tunnel --se-port $PORT -z 60 --logfile $HOME/logs/sc.log >> $HOME/logs/sc.log 2>&1 &
 
 echo "Tunnel launched on port $PORT"
 echo "Waiting for tunnel to bind to port..."
